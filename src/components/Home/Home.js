b28291f2941498836e9f460424916e2ba202ce77
John Whitten <johnwhitten.studio@gmail.com>
2024-02-02 17:02:48 -0800

    update landing page button

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,8 +5,8 @@ import NavBar from "../NavBar/NavBar"
 const Home = () => (
   <main>
     <NavBar />
-    <h2>Welcome to my portfolio!</h2>
-    <p>Here you will find information about my projects and experience.</p>
+    {/* <h2>Welcome to my portfolio!</h2>
+    <p>Here you will find information about my projects and experience.</p> */}
   </main>
 );
 
