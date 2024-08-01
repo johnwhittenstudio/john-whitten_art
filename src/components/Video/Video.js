1ba1b4728d6641703561a14a3dc501d078fadb94
John Whitten <johnwhitten.studio@gmail.com>
2024-07-31 17:09:58 -0700

    update/adjust some zoom styling in video gallery

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import NavBar from "../NavBar/NavBar";
 import classes from './Video.module.css';
-import bio from './../img/about.jpg';
+import bio from './../img/rock.png';
 import Zoom from 'react-medium-image-zoom';
 import 'react-medium-image-zoom/dist/styles.css';
 
@@ -19,7 +19,20 @@ const Video = () => (
       </header>
       <Zoom>
         <div className={classes.ImageContainer}>
-          <img src={bio} alt="Bio" style={{ width: '1000px', height: '667px' }} />
+          <img 
+            src={bio} 
+            alt="Bio" 
+            style={{ 
+              width: '1000px', 
+              height: '667px', 
+              // backgroundColor: '#000000',
+              backgroundPosition: '50%',
+              backgroundRepeat: 'no-repeat',
+              backgroundSize: 'cover',
+              // height: '50%',
+              // paddingBottom: '56%',
+              // width: '50%',
+            }} />
         </div>
       </Zoom>
     </div>
